e62ed69844087fc3b982c99e7c9bba2d8a72db55
phamtanphat <phatdroid94@gmail.com>
2021-10-27 20:45:58 +0700

    setup changenotifier

---
+++ lib/demo_changenotifier.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+class Count extends ChangeNotifier{
+  late int value;
+
+  Count({required this.value});
+}
+
+class DemoChangeNotifier extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => Count(value: 0),
+      child: DemoWidget(),
+    );
+  }
+}
+
+class DemoWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Count>(
+      builder: (context, count , child){
+        return Column(
+          children: [
+            Text("Count : ${count.value}"),
+            ElevatedButton(
+                onPressed: (){
+
+                },
+                child: Text("Increase count")
+            )
+          ],
+        );
+      },
+    );
+  }
+}
+
